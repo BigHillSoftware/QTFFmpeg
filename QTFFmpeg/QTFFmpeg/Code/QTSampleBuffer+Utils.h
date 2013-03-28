1b4b0bd90de6a1a250fa39317d28707f47696300
Brad O'Hearne <brado@bighillsoftware.com>
2013-03-28 00:18:17 -0700

    Got audio working, now audio video work but aren't synced.

---
+++ QTFFmpeg/QTFFmpeg/Code/QTSampleBuffer+Utils.h
@@ -0,0 +1,13 @@
+//
+//  QTSampleBuffer+Utils.h
+//  QTFFmpeg
+//
+//  Created by Brad O'Hearne on 3/26/13.
+//  Copyright (c) 2013 Big Hill Software LLC. All rights reserved.
+//
+
+#import <QTKit/QTKit.h>
+
+@interface QTSampleBuffer (Utils)
+
+@end
---
+++ QTFFmpeg/QTFFmpeg/Code/QTSampleBuffer+Utils.m
@@ -0,0 +1,13 @@
+//
+//  QTSampleBuffer+Utils.m
+//  QTFFmpeg
+//
+//  Created by Brad O'Hearne on 3/26/13.
+//  Copyright (c) 2013 Big Hill Software LLC. All rights reserved.
+//
+
+#import "QTSampleBuffer+Utils.h"
+
+@implementation QTSampleBuffer (Utils)
+
+@end
